81f980c30d6dd7e90d4058347f660d220a923e9c
ankahara <hecatombe100@gmail.com>
2024-02-05 16:59:45 -0600

    correccion

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import { createConnection } from "./db/connection";
 import { seed } from "./db/seed";
 import { runApp } from "./server";
 
-const start = async () => {
-    await createConnection({force: false})
-    // await seed()
+const start = async (seedDb = true) => {
+    await createConnection({force: seedDb})
+    if (seedDb) {
+        await seed()
+    }
     runApp({ port: 3000 })
 }
-
 start()
